2c73cab95177cf04432deda945d2a13b23c9cac1
Ousmana Traore <traoreosman@yahoo.fr>
2023-11-01 22:59:58 +0100

    key_split

---
+++ ARRAY/key_split.js
@@ -0,0 +1,11 @@
+// GET A aleatoire NUMBER BETWEEN 15 and 25 CARACTERE 
+let math = Math.floor(Math.random() * 10000) + 1;
+// GET A aleatoire STRING TEXT BETWEEN 15 and 25 CARACTERE 
+// exctrat a range caractere at the begining of number
+// exctrat a range caractere at the begining of string text
+// exctrat a range caractere at the middle of number
+// exctrat a range caractere at the middle of string text
+// exctrat a range caractere at the end of number
+// exctrat a range caractere at the end of string text
+// DELIVER THE METHOD of EXTRACTING/CHANGING for each place BEGINING|MIDDLE|END
+// SUBSTITUTE THE FIRST BEGINING|MIDDLE|END NUMBER EXTRACT TO BEGINING|MIDDLE|END STRING EXTRACT
