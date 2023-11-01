20aeaf8e6d9533f8e3099848d32a7983f886072e
Ousmana Traore <traoreosman@yahoo.fr>
2023-11-01 21:24:57 +0100

    array 2D method in javascript

---
+++ ARRAY/array_from_method.js
@@ -0,0 +1,3 @@
+const m = 4;
+const  n = 5;
+let arr = Array.from(Array(m), () => new Array(n));
---
+++ ARRAY/array_literal.js
@@ -0,0 +1,6 @@
+const m = 4;
+const  n = 5; // Note 2nd dimention is not relevant here
+let arr = [];
+for (var i = 0; i < m; i++) {
+  arr[i] = [];
+}
