24d9496b69338289dbfcd5af9611dca9380e012f
Ousmana Traore <traoreosman@yahoo.fr>
2023-11-26 21:55:56 +0100

    final version

--- CHROME_EXTENSION/index.js
+++ CHROME_EXTENSION/index.js
@@ -1,14 +1,63 @@
 
 let myLeads = [];
-let inputBtn = document.getElementById("input-btn");
-let inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const deleteAll = document.getElementById("delete-all");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn =document.getElementById("tab-btn");
+
+
+console.log(localStorage);
+// localStorage.clear();
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads);
+}
+const tabs  = [
+    {
+        url:"https://www.linkedin.com/in/ousmanatraore89"
+    }
+]
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true,currentWindow: true}, function(tabs){
+
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    });
+
+})
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value);
-    console.log(myLeads);
+    inputEl.value ="";
+    localStorage.setItem("myLead", JSON.stringify(myLeads));
+   render(myLeads);
+
+//    console.log(localStorage.getItem("myLead"));
 })
 
-for (let i= 0; i<myLeads.length; i++) {
-   ulEl.textContent +=myLeads[i] + "";
+deleteAll.addEventListener("dblclick", function(){
+    // myLeads = "";
+    // console.log("double clicked");
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+})
+function render(leads){
+
+    let lisItems = "";
+    for (let i= 0; i<leads.length; i++) {
+        // lisItems += "<li><a> target='_blank' href='" + myLeads[i] + "'>" ++ "</a></li>";
+        lisItems += `
+            <li>
+                <a href='${leads[i]}' target='_blank'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    } 
+    ulEl.innerHTML = lisItems
 }
